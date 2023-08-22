ef7840d9a565e492f9bb7ac2637c443d284775a4
ahmedSodagar1 <ahmed22sodagar@gmail.com>
2023-08-22 08:27:41 -0700

    done

---
+++ app.js
@@ -0,0 +1,9 @@
+// var arry = ["ali","moh.","gg" ]
+// var arry2 = arry.slice(0,2)
+// console.log(arry2)
+// console.log(arry)
+
+var a = +prompt("Enter Your Number");
+for(var i=0;i<5;i++){
+    console.log( "a X " + i + " " + "="+a*i);
+}
